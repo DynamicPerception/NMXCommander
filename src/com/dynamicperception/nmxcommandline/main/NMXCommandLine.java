03478b023abab081e897ef3276699eea0070dac4
Michael Ploof <michael@dynamicperception.com>
2015-12-10 12:34:19 -0500

    Switched from argument array to List

--- src/com/dynamicperception/nmxcommandline/main/NMXCommandLine.java
+++ src/com/dynamicperception/nmxcommandline/main/NMXCommandLine.java
@@ -7,7 +7,6 @@ import com.dynamicperception.nmxcommandline.coms.Serial;
 import com.dynamicperception.nmxcommandline.helpers.Console;
 import com.dynamicperception.nmxcommandline.models.Command;
 import com.dynamicperception.nmxcommandline.models.Command.Type;
-import com.dynamicperception.nmxcommandline.models.NMXCmd;
 import com.dynamicperception.nmxcommandline.models.NMXComs;
 
 public class NMXCommandLine {
@@ -42,98 +41,86 @@ public class NMXCommandLine {
 		String cmd = Console.getString();			
 		parseCommand(cmd);
 	}
+	
+	public static void parseCommand(String input){
+		parseCommand(getArgs(input));
+	}
 
-	public static void parseCommand(String input){		
+	public static void parseCommand(List<String> args){		
 		
-		String[] testArgs = getArgs(input);		
 		
-		// Find the command type
-		int endOfType = input.indexOf(" ");
-		endOfType = endOfType == -1 ? input.length() : endOfType;
-		String typeStr = input.substring(0, endOfType);
+		String typeStr = "";
+		String cmdStr = "";
 		
 		// Request to quit
-		if(testArgs[0].equals("exit")){
+		if(args.get(0).equals("exit")){
 			quit();
 		}
 		// Request to repeat command
-		else if(testArgs[0].equals("r")){			
-			input = input.substring(2, input.length());
-			int count = Integer.parseInt(input.substring(0, input.indexOf(DELIMITER)));			
-			input = input.substring(input.indexOf(DELIMITER)+1, input.length());			
+		else if(args.get(0).equals("r")){			
+			
+			int count = Integer.parseInt(args.get(1));			
+			for(int i = 0; i < 2; i++){
+				args.remove(0);
+			}
 			for(int i = 0; i < count; i++){
-				parseCommand(input);
+				parseCommand(args);
 			}			
 			return;
 		}
 		// Find command name
-		else if(testArgs[0].equals("find")){
-			String type = testArgs[1];
-			String term = testArgs[2];
-			if(type.equals("g")){
+		else if(args.get(0).equals("find")){
+			if(args.size() < 3){
+				Console.pln("Invalid search syntax");
+				return;
+			}
+		
+			
+			typeStr = args.get(1);
+			String term = args.get(2);
+			if(typeStr.equals("g")){
 				Command.Names.General.find(term);
 				return;
 			}
-			else if(type.equals("m")){
+			else if(typeStr.equals("m")){
 				Command.Names.Motor.find(term);
 				return;
 			}
-			else if(type.equals("c")){
+			else if(typeStr.equals("c")){
 				return;
 			}
-			else if(type.equals("k")){
+			else if(typeStr.equals("k")){
+				return;
+			}
+			else{
+				Console.pln("Invalid type");
 				return;
 			}
 		}
-		
-		// Find the command
-		String tmpCmd;
-		try{
-			tmpCmd = input.substring(endOfType+1, input.length());		
-		}catch(StringIndexOutOfBoundsException e){
-			// If there is no command, but there is a valid type, print the help for that type
-			if(typeStr.equals("m")){
-				Command.Names.Motor.help();
-			}
-			else if(typeStr.equals("g")){
+		// Help list
+		else if(args.size()== 1){
+			typeStr = args.get(0);
+			if(typeStr.equals("g")){
 				Command.Names.General.help();
+				return;
 			}
-			// Otherwise, this was a garbage command, so ignore it
-			return;
+			else if(typeStr.equals("m")){
+				Command.Names.Motor.help();;
+				return;
+			}
+			else if(typeStr.equals("c")){
+				return;
+			}
+			else if(typeStr.equals("k")){
+				return;
+			}
 		}
-		int endOfCmd = tmpCmd.indexOf(" ");
-		endOfCmd = endOfCmd == -1 ? tmpCmd.length() : endOfCmd;
-		String cmd = tmpCmd.substring(0, endOfCmd);
-		
-		if(endOfCmd == -1){			
-			NMXCmd.execute(typeStr, cmd, new ArrayList<Integer>());
-		}
-		
-		
-		// Find the number of arguments
-		tmpCmd = input.substring(endOfCmd+2, input.length());
-		int argCount = input.length() - input.replace(" ", "").length();	
-		
-		// Extract the arguments
-		List<Integer> args = new ArrayList<Integer>();
-		for(int i = 0; i < argCount; i++){
+		else{
+			typeStr = args.get(0);
 			
-			int firstIndex = tmpCmd.indexOf(DELIMITER) + 1;
-			int secondIndex = i == argCount-1 ? tmpCmd.length() : tmpCmd.indexOf(DELIMITER, firstIndex);
-			String arg = tmpCmd.substring(firstIndex, secondIndex);
-			try{
-				Integer argVal = Integer.parseInt(arg);
-				args.add(argVal);				
-				// Trim off the extracted argument
-				tmpCmd = tmpCmd.substring(secondIndex, tmpCmd.length());
-			}catch(NumberFormatException e){
-				// There was an extra space. We're done here.				
-				break;
-			}
+			if(args.size() > 1)
+				cmdStr = args.get(1);
 		}
-		
-		// Execute the command
-		//NMXCmd.execute(typeStr, cmd, args);
 		
 		Command.Type type = null;
 		if(typeStr.equals("g")){
@@ -149,21 +136,21 @@ public class NMXCommandLine {
 			type = Type.KEYFRAME;
 		}
 		try{			
-			Command thisCommand = Command.get(type, cmd);
+			Command thisCommand = Command.get(type, cmdStr);
 						
-			if(args.size() == 1 && args.get(0) == -1){
+			if(args.size() == 3 && args.get(2).equals("-h")){
 				thisCommand.printInfo();			
 				return;
 			}
 			
-			if(args.size() == 0){
+			if(args.size() == 2){				
 				thisCommand.execute();
 			}
-			else if(args.size() == 1){
-				thisCommand.execute(args.get(0));
+			else if(args.size() == 3){				
+				thisCommand.execute(args.get(2));
 			}
-			else if(args.size() == 2){
-				thisCommand.execute(args.get(0), args.get(1));
+			else if(args.size() == 4){				
+				thisCommand.execute(args.get(2), args.get(3));
 			}			
 		}catch(UnsupportedOperationException e){
 			Console.pln("Not a valid command");
@@ -171,16 +158,16 @@ public class NMXCommandLine {
 		}		
 	}
 	
-	private static String[] getArgs(String input){
+	private static List<String> getArgs(String input){		
 		int argCount = input.length() - input.replace(DELIMITER, "").length() + 1;
-		String[] args = new String[argCount];
+		List<String> args = new ArrayList<String>();
 		for(int i = 0; i < argCount; i++){
 			if(i == argCount-1){
-				args[i] = input;
+				args.add(input);
 			}
 			else{
 				int index = input.indexOf(DELIMITER);
-				args[i] = input.substring(0, index);
+				args.add(input.substring(0, index));
 				input = input.substring(index + 1, input.length());
 			}
 		}
