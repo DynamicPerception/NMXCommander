292724d42fc73e34d0e127a6deadc37816ca9980
Michael Ploof <michael@dynamicperception.com>
2016-01-14 18:02:19 -0500

    Human-readable macro support and documentation

--- src/com/dynamicperception/nmxcommandline/main/NMXCommandLine.java
+++ src/com/dynamicperception/nmxcommandline/main/NMXCommandLine.java
@@ -62,6 +62,9 @@ public class NMXCommandLine {
 		}		
 	}
 	
+	/**
+	 * Prints general application help
+	 */
 	private static void printHelp(){
 		Console.pln("\n\n******** NMX Commander Overview ********\n\n"
 				+ "This command line tool allows you to manually send single instuctions to the NMX controller.\n"
@@ -71,12 +74,16 @@ public class NMXCommandLine {
 				+ "Non-motor command types include \"g\" (general), \"c\" (camera), and \"k\" (key frame)\n"
 				+ "When specifying the motor number for motor commands, counting starts at 0 (i.e. valid motor #s are 0, 1, 2)\n\n"
 				+ "******** Other Useful Commands ********\n\n"
-				+ "\"<command type>.<command name>\" -h -- prints command-specific help\n"
-				+ "\"help\" -- prints this information again\n"
-				+ "\"list <command type>\" -- lists all commands of that type\n"
-				+ "\"find <command type>.<search term>\" -- returns all commands of that type containing the search term\n"
-				+ "\"exit\" -- closes the serial port and exits the application\n\n"
+				+ "* \"help\" -- prints this information again\n\n"
+				+ "* \"<command type>.<command name>\" -h -- prints command-specific help\n\n"
+				+ "* \"list <command type>\" -- lists all commands of that type\n\n"
+				+ "* \"find <command type>.<search term>\" -- returns all commands of that type containing the search term\n\n"
+				+ "* \"runMacro <path>\" -- runs a command macro list from a text file. Type \"runMacro\" without arguments\n"
+				+ "for macro file syntax\n\n"
+				+ "* \"exit\" -- closes the serial port and exits the application\n\n"
 				+ "******** Some Important Tips ********\n\n"
+				+ "* A boolean (true/false) value is indicated by a data value of 0 or 1. For example, the command to enable the camera\n"
+				+ "would be \"c.setEnable 1\".\n\n"
 				+ "* Valid microstep settings are 4, 8, and 16. The NMX does not use full or half steps in order to reduce vibation.\n\n"
 				+ "* The controller accepts and returns step counts that correspond to relevant motor's current microstep setting.\n"
 				+ "If the NMX reports a motor position of 500 when the microstep setting is 4, it will report 1000 when the microstep\n"
@@ -115,7 +122,7 @@ public class NMXCommandLine {
 	/**
 	 * Retrieves input from the console and passes it to command parser
 	 */
-	public static void getCommand(){
+	private static void getCommand(){
 		Console.p("\nCmd: ");
 		String cmd = Console.getString();			
 		parseCommand(cmd);
@@ -126,7 +133,7 @@ public class NMXCommandLine {
 	 * overloaded parseCommand() that accepts a List of arguments.
 	 * @param input Command string
 	 */
-	public static void parseCommand(String input){
+	private static void parseCommand(String input){
 		parseCommand(getArgs(input, DELIMITER));
 	}
 
@@ -134,7 +141,7 @@ public class NMXCommandLine {
 	 * Handles any application specific commands, then attempts to execute any NMX commands
 	 * @param args A List of String arguments that comprise the command
 	 */
-	public static void parseCommand(List<String> args){		
+	private static void parseCommand(List<String> args){		
 		
 		// Request to quit
 		if(args.get(0).equals("exit")){
@@ -150,10 +157,34 @@ public class NMXCommandLine {
 		// Run command list from file
 		else if(args.get(0).equals("runMacro")){
 			try {
-				if(args.size() == 1)
-					runCommandFile(Paths.get("c:/commandList.txt"));
+				if(args.size() == 1){
+					Console.pln("\nrunMacro syntax -- \"runMacro <macro path>\"");
+					Console.pln("Example -> \"runMacro c:\\NMXmacro.txt\"\n");
+					Console.pln("The text file should have one command on each line with the following syntax:\n\n"
+							+ "\"<delay in milliseconds after last command> <command type>.<command name> <data OR motor #> <motor cmd data>\"\n\n"
+							+ "The following example enables the camera, sets the focus time to 600ms, trigger time to 100ms, sets home\n"
+							+ "for each the motors (e.g. sets current position to 0), immediately takes an exposure, commands the motors to\n"
+							+ "a new position, waits 5000ms, takes another exposure, waits 1000ms, commands the motors back to their original\n"
+							+ "positions, waits 5000ms, then takes a final exposure:\n\n"
+							+ "0 c.setEnable 1\n"
+							+ "0 c.setFocus 600 \n"
+							+ "0 c.setTrigger 100 \n"
+							+ "0 m.setHome 0\n"
+							+ "0 m.setHome 1\n"
+							+ "0 m.setHome 2\n"
+							+ "0 c.expose\n"
+							+ "0 m.sendTo 0 15000\n"
+							+ "0 m.sendTo 1 2560\n"
+							+ "0 m.sendTo 2 -5000\n"
+							+ "5000 c.expose\n"
+							+ "1000 m.sendTo 0 0\n"
+							+ "0 m.sendTo 1 0\n"
+							+ "0 m.sendTo 2 0\n"
+							+ "5000 c.expose\n\n"
+							+ "Lines in the macro file may be commented out by starting a line with \"//\"");
+				}
 				else if(args.size() > 1){
-					runCommandFile(Paths.get(args.get(1)));
+					runMacro(Paths.get(args.get(1)));
 				}				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -290,7 +321,7 @@ public class NMXCommandLine {
 	 * @param path Path of the macro text file
 	 * @throws IOException
 	 */
-	private static void runCommandFile(Path path) throws IOException {		
+	private static void runMacro(Path path) throws IOException {		
 		
 		// Get the list of commands
 		final Charset ENCODING = StandardCharsets.UTF_8;
@@ -298,7 +329,17 @@ public class NMXCommandLine {
 		
 		// Run them
 		for(int i = 0; i < commands.size(); i++){
-			parseCommand(commands.get(i));
+			List<String> commandArgs = getArgs(commands.get(i), DELIMITER);
+			// Skip blank or commented lines			
+			if((commandArgs.get(0).trim().length() == 0) || commandArgs.get(0).indexOf("//") >= 0){
+				continue;
+			}
+			// Wait the requested delay
+			wait(Integer.parseInt(commandArgs.get(0)));
+			// Remove the delay time from the argument list
+			commandArgs.remove(0);
+			// Pass the remaining arguments to the command parser
+			parseCommand(commandArgs);
 		}		
 	}
 	
@@ -345,8 +386,7 @@ public class NMXCommandLine {
 			}
 			
 			if(ignore)
-				continue;
-				
+				continue;				
 			
 			long time = Long.parseLong(args.get(1));
 			int _address = Integer.parseInt(args.get(2));
@@ -380,6 +420,23 @@ public class NMXCommandLine {
 				System.exit(0);				
 			}
 		}		
+	}
+	
+	/**
+	 * Waits a given amount of time and prints a countdown of time remaining
+	 * @param waitTime Time to wait in milliseconds
+	 */
+	private static void wait(int waitTime){
+		long waitStart = System.currentTimeMillis();
+		long lastUpdate = waitStart;
+		int updateRate = 1000;
+		while(System.currentTimeMillis() - waitStart < waitTime){
+			// If it's a long wait, let the user know how much time remains
+			if(System.currentTimeMillis() - lastUpdate > updateRate){
+				Console.pln("Time till next command: " + (waitTime - (System.currentTimeMillis() - waitStart)) + " ms");
+				lastUpdate = System.currentTimeMillis();
+			}
+		}
 	}
 	
 	/**
