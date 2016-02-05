c9401466b49f025c9b758aa3bbef083fa4036963
Michael Ploof <michael@dynamicperception.com>
2016-02-05 13:59:11 -0500

    Command name fix, added g.getFreeMemory command

--- src/com/dynamicperception/nmxcommandline/models/Command.java
+++ src/com/dynamicperception/nmxcommandline/models/Command.java
@@ -387,7 +387,7 @@ public class Command {
 			public static final String GET_ACCEL_AT		= "k.getAccelAt";			
 			public static final String IS_VEL_VALID		= "k.isVelValid";
 			public static final String IS_ACCEL_VALID	= "k.isAccelValid";
-			public static final String GET_CONT_VID_DUR = "k.getContVidDur";
+			public static final String GET_CONT_VID_TIME = "k.getContVidTime";
 			public static final String GET_RUN_STATE 	= "k.getRunState";
 			public static final String GET_RUN_TIME		= "k.getRunTime";
 			public static final String GET_MAX_RUN_TIME = "k.getMaxRunTime";
@@ -632,6 +632,7 @@ public class Command {
 		generalList.add(new Command(Command.Type.GENERAL, 129, Boolean.class, Names.General.IS_PROGRAM_VALID));
 		generalList.add(new Command(Command.Type.GENERAL, 130, Integer.class, Names.General.GET_MOTOR_SLEEP_STATES));
 		generalList.add(new Command(Command.Type.GENERAL, 150, Boolean.class, Names.General.IS_GRAFFIK));
+		generalList.add(new Command(Command.Type.GENERAL, 200, Integer.class, Names.General.GET_FREE_MEMORY));
 		
 		Command setDebug = new Command(Command.Type.GENERAL, 254, Names.General.SET_DEBUG, Byte.class);		
 		setDebug.setHelpCommand(new DebugHelp());
@@ -789,7 +790,7 @@ public class Command {
 		keyFrameList.add(new Command(Command.Type.KEYFRAME, 104, Float.class, Names.KeyFrame.GET_ACCEL_AT, Float.class));
 		keyFrameList.add(new Command(Command.Type.KEYFRAME, 105, Names.KeyFrame.IS_VEL_VALID));
 		keyFrameList.add(new Command(Command.Type.KEYFRAME, 106, Boolean.class, Names.KeyFrame.IS_ACCEL_VALID));
-		keyFrameList.add(new Command(Command.Type.KEYFRAME, 107, Integer.class, Names.KeyFrame.GET_CONT_VID_DUR));		
+		keyFrameList.add(new Command(Command.Type.KEYFRAME, 107, Integer.class, Names.KeyFrame.GET_CONT_VID_TIME));		
 
 		keyFrameList.add(new Command(Command.Type.KEYFRAME, 120, Integer.class, Names.KeyFrame.GET_RUN_STATE));
 		keyFrameList.add(new Command(Command.Type.KEYFRAME, 121, Long.class, Names.KeyFrame.GET_RUN_TIME));
