647edac02fd7acdfc72fb6ee91747d5bcb56eb4b
mploof <jayburlage@Jays-MacBook-Air.local>
2016-11-30 19:36:01 -0500

    Added file print method, comments

--- src/com/dynamicperception/nmxcommandline/helpers/Console.java
+++ src/com/dynamicperception/nmxcommandline/helpers/Console.java
@@ -1,19 +1,37 @@
 package com.dynamicperception.nmxcommandline.helpers;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Console {
 
+    /**
+     * Convenience print method
+     * 
+     * @param output
+     *            Object to be printed to console
+     */
     public static void p(Object output) {
         System.out.print(output);
     }
 
+    /**
+     * Convenience print method with new line
+     * 
+     * @param output
+     *            Object to be printed to console
+     */
     public static void pln(Object output) {
         System.out.println(output);
     }
 
+    /**
+     * Gets string input from user in console
+     * 
+     * @return The input as a string
+     */
     public static String getString() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
@@ -26,6 +44,11 @@ public class Console {
         }
     }
 
+    /**
+     * Gets integer input from user in console
+     * 
+     * @return The input as an integer
+     */
     public static int getInteger() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
@@ -34,6 +57,23 @@ public class Console {
         } catch (NumberFormatException | IOException nfe) {
             System.err.println("Invalid Format!");
             return Consts.ERROR;
+        }
+    }
+
+    /**
+     * Prints the contents of a file in the console
+     * 
+     * @param filePath
+     *            The path of the file to be printed
+     */
+    public static void printFile(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Attempted to print file from invalid path!!!");
         }
     }
 }
